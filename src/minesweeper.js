3fcacf1bfa1dd69faa1bbed1cb204cd491c639c7
Dave J <daveswork@users.noreply.github.com>
2017-09-07 16:04:43 -0400

    Completed second minesweeper tasks.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,22 @@
+const printBoard = (board) =>{
+    console.log("Current Board:");
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
+let board = [];
 
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+board = [
+[" ", " ", " "], 
+[" ", " ", " "], 
+[" ", " ", " "]
+];
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+printBoard(board);
 
-console.log('This is what a board with a guess and a bomb on it would look like: ');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+board[0][1] = '1';
+board[2][2] = 'B';
 
+printBoard(board);
 
